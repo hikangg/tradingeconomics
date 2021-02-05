8a02878ee41d070b2418ea979aadae5fce1e7ff4
hikangg <kangg1987@hotmail.com>
2021-02-06 02:34:01 +0800

    change api endpoint

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <html>
-<body>
-
-<form action="send.php" method="post">
-Country 1: <select id="first" name="first">
-    <option value="sweden">Sweden</option>
-    <option value="mexico">Mexico</option>
-    <option value="thailand">Sweden</option>
-</select><br>
-Country 2: <select id="second" name="second">
-    <option value="sweden">Sweden</option>
-    <option value="mexico">Mexico</option>
-    <option value="thailand">Sweden</option>
-</select><br>
-<input type="submit">
-</form>
-
-</body>
+    <head>
+        <title>Trading Economics</title>
+    </head>
+    <body>
+        <form action="send.php" method="post">
+            Country 1: <select id="first" name="first">
+                <option value="Sweden">Sweden</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Thailand">Thailand</option>
+                <option value="New Zealand">New Zealand</option>
+            </select><br>
+            Country 2: <select id="second" name="second">
+                <option value="Sweden">Sweden</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Thailand">Thailand</option>
+                <option value="New Zealand">New Zealand</option>
+            </select><br>
+            <input type="submit">
+        </form>
+    </body>
 </html>
--- send.php
+++ send.php
@@ -1,6 +1,19 @@
-<?php            
-	function getGdpByCountry($country) {
-		$url = 'https://api.tradingeconomics.com/country/'.$country.'/gdp';
+<?php
+	$display_dataset = [
+		'GDP', 
+		'GDP Annual Growth Rate', 
+		'GDP Constant Prices', 
+		'GDP From Agriculture', 
+		'GDP From Construction', 
+		'GDP From Manufacturing',
+		'GDP From Mining',
+		'GDP From Public Administration',
+		'GDP From Services',
+		'GDP From Transport',
+		'GDP From Utilities'];
+
+	function getDataByCountry($country) {
+		$url = 'https://api.tradingeconomics.com/country/'.$country;
 		$headers = array(
 			"Accept: application/xml",
 			"Authorization: Client 0u8z6so9rqva9tp:laguhrtr813pidw"
@@ -16,75 +29,68 @@
 		$data = json_decode($data, true);
 		return $data;
 	}
-	
-	function getGDPAnnualGrowthRate($country) {
-		$url = 'https://api.tradingeconomics.com/country/'.$country.'/gdp annual growth rate';
-		$headers = array(
-			"Accept: application/xml",
-			"Authorization: Client 0u8z6so9rqva9tp:laguhrtr813pidw"
-		);
-		$handle = curl_init(); 
-		curl_setopt($handle, CURLOPT_URL, $url);
-		curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 
-		$data = curl_exec($handle);
-		curl_close($handle);
-
-		$data = json_decode($data, true);
-		return $data;
-	}
-
-	$first_gdp_data = getGdpByCountry($_POST['first']);
-	$second_gdp_data = getGdpByCountry($_POST['second']);
-
-	$first_gdp_growth_rate_data = getGDPAnnualGrowthRate($_POST['first']);
-	$second_gdp_growth_rate_data = getGDPAnnualGrowthRate($_POST['second']);
+	$first_data = getDataByCountry($_POST['first']);
+	$second_data = getDataByCountry($_POST['second']);
 ?>
 
 <html>
-<head>
-<style>
-table {
-  width:100%;
-}
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 15px;
-  text-align: left;
-}
-#t01 tr:nth-child(even) {
-  background-color: #eee;
-}
-#t01 tr:nth-child(odd) {
- background-color: #fff;
-}
-#t01 th {
-  background-color: black;
-  color: white;
-}
-</style>
-</head>
-<body>
-<table id="t01">
-  <tr>
-    <th>Category</th>
-    <th><?php echo($_POST['first']) ?></th> 
-    <th><?php echo($_POST['second']) ?></th>
-  </tr>
-  <tr>
-    <td>GDP</td>
-    <td><?php echo($first_gdp_data[0]['LatestValue']) ?></td>
-    <td><?php echo($second_gdp_data[0]['LatestValue']) ?></td>
-  </tr>
-  <tr>
-    <td>GDP Annual Growth Rate</td>
-    <td><?php echo($first_gdp_growth_rate_data[0]['LatestValue']) ?></td>
-    <td><?php echo($second_gdp_growth_rate_data[0]['LatestValue']) ?></td>
-  </tr>
-</table>
-</body>
+	<head>
+		<title>Trading Economics</title>
+		<style>
+			table {
+				width:100%;
+			}
+			table, th, td {
+				border: 1px solid black;
+				border-collapse: collapse;
+			}
+			th, td {
+				padding: 15px;
+				text-align: left;
+			}
+			#t01 tr:nth-child(even) {
+				background-color: #eee;
+			}
+			#t01 tr:nth-child(odd) {
+				background-color: #fff;
+			}
+			#t01 th {
+				background-color: black;
+				color: white;
+			}
+		</style>
+	</head>
+	<body>
+		<table id="t01">
+			<tr>
+				<th>Category</th>
+				<th><?php echo($_POST['first']) ?></th> 
+				<th><?php echo($_POST['second']) ?></th>
+			</tr>
+			<?php
+				for($i=0; $i<count($display_dataset); $i++) {
+					$first['LatestValue'] = 0;
+					$second['LatestValue'] = 0;
+					for($k=0; $k<count($first_data); $k++) {
+						if($first_data[$k]['Category'] == $display_dataset[$i]) {
+							$first = $first_data[$k];
+						}
+					}
+
+					for($k=0; $k<count($second_data); $k++) {
+						if($second_data[$k]['Category'] == $display_dataset[$i]) {
+							$second = $second_data[$k];
+						}
+					}
+					echo('<tr>
+						<td>'.$display_dataset[$i].'</td>
+						<td>'.$first['LatestValue'].'</td>
+						<td>'.$second['LatestValue'].'</td>
+					</tr>');
+				}
+			?>
+		</table>
+		<a href="index.php">Return</a>
+	</body>
 </html>
